f72b234f2468cc4f4671c98588784e57a12237f6
Camille GIRARD-NICOLAS <cgirard-nicolas@sciado.onmicrosoft.com>
2021-12-26 00:08:55 +0100

    modif tests.js

--- tests.js
+++ tests.js
@@ -14,6 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 const allSockets = [];
+let usersAvailable = [];
 let sizeUsersAvailable = 0;
 
 mongoose.connect(process.env.DB_ADDRESS);
